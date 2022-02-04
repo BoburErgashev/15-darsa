861cd53515eef8ddcf4d5e882c9a6039dbefb692
Bobur <boburergashev946@gmail.com>
2022-02-04 22:07:06 +0500

    Update 15-dars.py

--- 15-dars.py
+++ 15-dars.py
@@ -4,64 +4,80 @@ Created on Wed Jan 19 21:49:42 2022
 
 @author: Bobur
 """
-talaba_0 = {
-    'ism':'alijon',
-    'familiya':'shamshiyev',
-    'yosh':22,
-    'fakultet':'matematika',
-    'kurs':4
-      }
-izohli_lugat={
-    "boolean":"mantiqiy qiymat",
-    "float":"o\'nlik son",
-    "for":"biror narsani qayta qayta bajarish sikli",
-    "If":"shartlarni tekshirish",
-    "Integer":"butun son",
-    "len":"qatorni aniqlash",
-    "toople":"o\'zgarmas royhat",
-    "title":"so\'zni bosh harfini kattada boshlaydi",
-    "little":"so\'zdagi hamma harflarni kichkina qiladi",
-    "upper":"so\'zlarni kotta harfga aylantiradi",
-    "range":"sonlarni ketmaketligini to\'g\'irlaydi"
-    }
-print("Pythin elementlar ro'yxati:")
-for k,v in sorted(izohli_lugat.items()):
-    print(f"{k.title()}-{v}")
-davlatlar={
-    "avg\'oniston":"kobul",
-    "qozog\'iston":"nursultan",
-    "qirg\'iziston":"kesh",
-    "o\'zbeksistn":"toshkent",
-    "aqsh":"vashington",
-    "buyuk britanya":"london"}
-for key in sorted(davlatlar.keys()):
-    print(key.upper())
-for value in sorted(davlatlar.values()):
-    print(value.title())
-country=input("Qaysi davlat haqida bilishni hohlaysiz?\n Istalgan davlatni kiriting>>>")
-capital=davlatlar.get(country)
-if capital==None:
-    print("Bizda bunday malumot yo\'q")
-else:
-    print(f"{country.upper()} davlatining poytaxti {capital.upper()} shaxrti!!!")
-menu={
-      "somsa go\'shtli":3000,
-      "soms tovuqli":2500,
-      "ko\'k somsa":7000,
-      "lag\'mon":5000,
-      "chuchvara":90000,
-      "kartoshka pyure":100000,
-      "tovuq go\'sht":2000}
-print("3 taom buyurtnma qiling.")
-buyurtmalar=[]
-for n in range(3):
-    buyurtmalar.append(input(f"{n+1}- taom:").lower())
-for buyurtma in buyurtmalar:
-    if buyurtma in menu:
-        print(f"{buyurtma.title()} {menu[buyurtma]} so\'m")
-    else:
-         print(f"Kechirasiz bizda {buyurtma} yo\'q ")
-    
+# talaba_0 = {
+#     'ism':'alijon',
+#     'familiya':'shamshiyev',
+#     'yosh':22,
+#     'fakultet':'matematika',
+#     'kurs':4
+#       }
+# izohli_lugat={
+#     "boolean":"mantiqiy qiymat",
+#     "float":"o\'nlik son",
+#     "for":"biror narsani qayta qayta bajarish sikli",
+#     "If":"shartlarni tekshirish",
+#     "Integer":"butun son",
+#     "len":"qatorni aniqlash",
+#     "toople":"o\'zgarmas royhat",
+#     "title":"so\'zni bosh harfini kattada boshlaydi",
+#     "little":"so\'zdagi hamma harflarni kichkina qiladi",
+#     "upper":"so\'zlarni kotta harfga aylantiradi",
+#     "range":"sonlarni ketmaketligini to\'g\'irlaydi"
+#     }
+# print("Pythin elementlar ro'yxati:")
+# for k,v in sorted(izohli_lugat.items()):
+#     print(f"{k.title()}-{v}")
+# davlatlar={
+#     "avg\'oniston":"kobul",
+#     "qozog\'iston":"nursultan",
+#     "qirg\'iziston":"kesh",
+#     "o\'zbeksistn":"toshkent",
+#     "aqsh":"vashington",
+#     "buyuk britanya":"london"}
+# for key in sorted(davlatlar.keys()):
+#     print(key.upper())
+# for value in sorted(davlatlar.values()):
+#     print(value.title())
+# country=input("Qaysi davlat haqida bilishni hohlaysiz?\n Istalgan davlatni kiriting>>>")
+# capital=davlatlar.get(country)
+# if capital==None:
+#     print("Bizda bunday malumot yo\'q")
+# else:
+#     print(f"{country.upper()} davlatining poytaxti {capital.upper()} shaxrti!!!")
+# menu={
+#       "somsa go\'shtli":3000,
+#       "soms tovuqli":2500,
+#       "ko\'k somsa":7000,
+#       "lag\'mon":5000,
+#       "chuchvara":90000,
+#       "kartoshka pyure":100000,
+#       "tovuq go\'sht":2000}
+# print("3 taom buyurtnma qiling.")
+# buyurtmalar=[]
+# for n in range(3):
+#     buyurtmalar.append(input(f"{n+1}- taom:").lower())
+# for buyurtma in buyurtmalar:
+#     if buyurtma in menu:
+#         print(f"{buyurtma.title()} {menu[buyurtma]} so\'m")
+#     else:
+#          print(f"Kechirasiz bizda {buyurtma} yo\'q ")
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310'
+#     }
+
+# for k, q in telefonlar.items():
+#     print(f"{k.title()}ning telefoni {q}")   
+qimatli_qogozlar={
+    'Ameriika':'dollor',
+    'Britanya':'yevro',
+    'Avstraliya':'avstriya dollori',
+    'Tojikiston':'somoniy',
+    'Qozog\'iston':'tenge'}
+for kalit,qiymat in qimatli_qogozlar.items():
+    print(f"{kalit.title()}ning pul birligi {qiymat}")
     
     
     
